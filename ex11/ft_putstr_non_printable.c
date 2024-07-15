56d175463273dc5fc7f2fa683f0da1caaac9b32d
JariJariLo <jarijarilopez@gmail.com>
2024-07-15 16:50:27 +0200

    modificacion

--- ex11/ft_putstr_non_printable.c
+++ ex11/ft_putstr_non_printable.c
@@ -10,9 +10,10 @@ void hex_conv(char str) {
 
     write(1, &prefix, 1);
 
-    char temp = (str >> 4) & 0xF; 
+    char temp = (str >> 4) & 0xF; // >>4 te mueve a la derecha a la posición 4 de los bits 
+    //para convertir a hexadecimal y luego realiza la operación AND con la máscara 0000000011111111
     if (temp < 10) {
-        n = '0' + temp;
+        n = '0' + temp;//convierte a char
     } else {
         n = 'a' + temp - 10;
     }
@@ -31,7 +32,7 @@ void ft_putstr_non_printable(char *str) {
     int i = 0;
     while (str[i] != '\0') {
         if (!is_printable(str[i])) {
-            hex_conv(str[i]);
+            hex_conv(str[i]);//llama a la función para conversión
         } else {
             write(1, &str[i], 1);
         }
@@ -40,7 +41,7 @@ void ft_putstr_non_printable(char *str) {
 }
 
 int main() {
-    char str[] = "Hello\nWorld\t!";
+    char str[] = "Hello\nWorld\tbo\v!";
     ft_putstr_non_printable(str);
     write(1, "\n", 1);
     return 0;
