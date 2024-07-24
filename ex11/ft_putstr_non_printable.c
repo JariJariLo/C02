c35160f295b0ec016f649d259251a1cd534b59c3
JariJari <146730448+JariJariLo@users.noreply.github.com>
2024-07-24 10:13:10 +0200

    Update ft_putstr_non_printable.c

--- ex11/ft_putstr_non_printable.c
+++ ex11/ft_putstr_non_printable.c
@@ -9,63 +9,37 @@
 /*   Updated: 2024/07/24 01:24:53 by crjarill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include <unistd.h>
 
-int	is_printable(char c)
-{
-	return (c >= 32 && c <= 126);
+int is_printable(char c) {
+    return (c >= 32 && c <= 126);
 }
 
-void	hex_conv(char str)
-{
-	char	n;
-	char	prefix;
-	char	temp;
+void hex_conv(char str) {
+    char n;
+    char temp;
 
-	prefix = '\\';
-	write(1, &prefix, 1);
-	temp = (str >> 4) & 0xF;
-	if (temp < 10)
-	{
-		n = '0' + temp;
-	}
-	else
-	{
-		n = 'a' + temp - 10;
-	}
-	write(1, &n, 1);
-	temp = str & 0xF;
-	if (temp < 10)
-	{
-		n = '0' + temp;
-	}
-	else
-	{
-		n = 'a' + temp - 10;
-	}
-	write(1, &n, 1);
+    write(1, "\\", 1);
+
+    temp = (str >> 4) & 0xF;
+    n = temp < 10 ? '0' + temp : 'a' + temp - 10;
+    write(1, &n, 1);
+
+    temp = str & 0xF;
+    n = temp < 10 ? '0' + temp : 'a' + temp - 10;
+    write(1, &n, 1);
 }
 
-void	ft_putstr_non_printable(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i] != '\0')
-	{
-		if (!is_printable(str[i]))
-		{
-			hex_conv(str[i]);
-		}
-		else
-		{
-			write(1, &str[i], 1);
-		}
-		i++;
-	}
+void ft_putstr_non_printable(char *str) {
+    int i = 0;
+    while (str[i]) {
+        if (is_printable(str[i]))
+            write(1, &str[i], 1);
+        else
+            hex_conv(str[i]);
+        i++;
+    }
 }
-
 /*int	main(void)
 {
 	char	str[] = "Hello\nWorld\tbo\v!";
